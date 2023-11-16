aae4fb32f1a4ef6fe722aa2d8b30b7bc4272c591
raflial75 <121711090+raflial75@users.noreply.github.com>
2023-11-16 09:26:28 +0700

    Delete data directory

--- data/database.js
+++
@@ -1,9 +0,0 @@
-const mysql  = require('mysql8')
-const connection = mysql.createConnection({
-    host        : 'localhost',
-    user        : 'root',
-    password    : 'Basisdata-my-sql-1st',
-    database    : 'new_schema'
-});
-
-exports.connection = connection;
