79bc5a00bd35b16f5a9ed019b9c32ac381ad0b1f
alfian <raflial00750@gmail.com>
2023-11-21 08:34:25 +0700

    make email and username can only used for one users

--- features/register/register.js
+++ features/register/register.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const {nanoid} = require('nanoid')
+const express   = require('express')
+const bcyript   = require ('bcrypt')
+const router    = express.Router()
+
 const { connection } = require('../../data/database')
 
+//connect into database
 connection.connect(err => {
     if(err){
         console.error('Error connecting to MySQL:', err)
@@ -11,21 +13,41 @@ connection.connect(err => {
     console.log('Connected to MySQL database')
 })
 
+//send POST request to register new users
 router.post('/register', (req, res) => {
-    const userId = nanoid(16)
+    const saltRounds = 10
     const {username, email, password} = req.body
-    const query  = 'INSERT INTO batiqu_users_data (username, email, password, userId) VALUES (?, ?, ?, ?)'
-    const values = [username, email, password, userId]
-
-    connection.query(query, values, (err, result) => {
-        if(err){
-            console.error('An error occurred while inserting user data:', err)
-            res.status(500).send({message : 'There was a problem when adding user'})
-            return
+    
+    //check the username or email that already in use
+    const checkQuery = 'SELECT * FROM batiqu_users_data WHERE username = ? OR email = ?'
+    connection.query(checkQuery, [username, email], (checkErr, checkResult) => {
+        if(checkErr){
+            console.error('An error occured while checking email/username', checkErr)
+            return res.status(500).send({message : 'There was problem on internal server'})
         }
 
-        console.log('User data insterted successfully');
-        res.status(200).send({message: 'User added successfully'}) 
+        if(checkResult.length > 0){
+            console.log('user data already in use', checkResult)
+            return res.status(409).send({message : 'Username or Email already in use'})
+        }
+        
+        //hashing the password
+        bcyript.hash(password, saltRounds, function(err, hash){
+            const query     = 'INSERT INTO batiqu_users_data (username, email, password) VALUES (?, ?, ?)'
+            const values    = [username, email, hash]
+            
+            //store new users data to database
+            connection.query(query, values, (err, result) => {
+                if(err){
+                    console.error('An error occurred while inserting user data:', err)
+                    res.status(500).send({message : 'There was a problem when adding user'})
+                    return
+                }
+        
+                console.log('User data inserted successfully')
+                res.status(200).send({message: 'User added successfully'})
+            })
+        })
     })
 })
 
